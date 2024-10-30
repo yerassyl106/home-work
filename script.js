ccf4cc46988cdbf351f5e0be9ba9011ed9fcc524
yerassyl daurengazyuly <yerassyl106@gmail.com>
2024-10-30 22:21:56 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+var modelName = "..."
+var color = "..."
+var  wheelsCount = "..."
+var isOffRoad = "..."
+var rating = "..."
+
+
+console.log("Daewoo Matiz",modelName);
+console.log("yellowgreen",color);
+console.log("four-wheels",wheelsCount);
+console.log("Арналмаған",isOffRoad); 
+console.log("7/10",rating);
+
+
+
+
